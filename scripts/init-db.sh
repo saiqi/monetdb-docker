7e7d3825f9be7d40cdea2e51b2657d902bba27d1
julien <julien.bernard.iphone@gmail.com>
2016-07-23 21:16:17 +0200

    with R support

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-chown -R monetdb:monetdb /var/monetdb5/
-
-if [ ! -d "/var/monetdb5/dbfarm" ]; then
-   monetdbd create /var/monetdb5/dbfarm
-else
-    echo "Existing dbfarm found in '/var/monetdb5/dbfarm'"
-fi
-
 monetdbd start /var/monetdb5/dbfarm
 
 sleep 5
@@ -18,24 +10,5 @@ if [ ! -d "/var/monetdb5/dbfarm/db" ]; then
 else
     echo "Existing database found in '/var/monetdb5/dbfarm/db'"
 fi
-
-for i in {30..0}; do
-    echo 'Testing MonetDB connection ' $i
-    mclient -d db -s 'SELECT 1' &> /dev/null
-    if [ $? -ne 0 ] ; then
-      echo 'Waiting for MonetDB to start...'
-      sleep 1
-    else
-        echo 'MonetDB is running'
-        break
-    fi
-done
-if [ $i -eq 0 ]; then
-    echo >&2 'MonetDB startup failed'
-    exit 1
-fi
-
-mkdir -p /var/log/monetdb
-chown -R monetdb:monetdb /var/log/monetdb
 
 echo "Initialization done"
